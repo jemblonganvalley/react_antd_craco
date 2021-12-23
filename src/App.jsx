ba8add354043c167091a3454a57fd0ae27d4737f
jemblonganvalley <jemblonganvalley@gmail.com>
2021-12-23 11:32:45 +0700

    first update

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.less";
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button type="primary">hello</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
